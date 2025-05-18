4ff14f5eac47c46b2500168925e2c5f311f661f8
kapkaaa <kafkasanjaya123360@gmail.com>
2025-05-18 18:40:41 +0700

    idk

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.AplikasiParkir.Staff
+            Me.MainForm = Global.AplikasiParkir.Login
         End Sub
     End Class
 End Namespace
