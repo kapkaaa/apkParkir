b1df3580713d8030920bad4096522e02964e1c4a
kapkaaa <kafkasanjaya123360@gmail.com>
2025-05-18 18:39:42 +0700

    Fix session UI Security

--- Security.vb
+++ Security.vb
@@ -1,11 +1,9 @@
 ﻿Public Class Security
     Private Sub btnTRS_Click(sender As Object, e As EventArgs) Handles btnTRS.Click
         transaksi.Show()
-        Me.Hide()
     End Sub
 
     Private Sub btnLPRN_Click(sender As Object, e As EventArgs) Handles btnLPRN.Click
         Laporan.Show()
-        Me.Hide()
     End Sub
 End Class
