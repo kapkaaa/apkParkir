1c0c71beb36150dd4f5cfa1db729b693a1bc2ffa
kapkaaa <kafkasanjaya123360@gmail.com>
2025-05-23 02:42:31 +0700

    Adding form Motor for user input

---
+++ user.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Motor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Motor))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CRUD = New System.Windows.Forms.GroupBox()
+        Me.dtpTime = New System.Windows.Forms.DateTimePicker()
+        Me.dtpDay = New System.Windows.Forms.DateTimePicker()
+        Me.btnBTL = New System.Windows.Forms.Button()
+        Me.btnSV = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tbNO = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbHRG = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.CRUD.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(75, Byte), Integer), CType(CType(115, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 100)
+        Me.Panel1.TabIndex = 17
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Rockwell", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Location = New System.Drawing.Point(100, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(252, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Transaksi Parkir Motor"
+        '
+        'CRUD
+        '
+        Me.CRUD.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CRUD.Controls.Add(Me.tbHRG)
+        Me.CRUD.Controls.Add(Me.Label2)
+        Me.CRUD.Controls.Add(Me.dtpTime)
+        Me.CRUD.Controls.Add(Me.dtpDay)
+        Me.CRUD.Controls.Add(Me.btnBTL)
+        Me.CRUD.Controls.Add(Me.btnSV)
+        Me.CRUD.Controls.Add(Me.Label6)
+        Me.CRUD.Controls.Add(Me.Label5)
+        Me.CRUD.Controls.Add(Me.tbNO)
+        Me.CRUD.Controls.Add(Me.Label3)
+        Me.CRUD.Location = New System.Drawing.Point(222, 142)
+        Me.CRUD.Name = "CRUD"
+        Me.CRUD.Size = New System.Drawing.Size(543, 404)
+        Me.CRUD.TabIndex = 19
+        Me.CRUD.TabStop = False
+        Me.CRUD.Text = "Form"
+        '
+        'dtpTime
+        '
+        Me.dtpTime.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dtpTime.Enabled = False
+        Me.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.dtpTime.Location = New System.Drawing.Point(139, 31)
+        Me.dtpTime.Name = "dtpTime"
+        Me.dtpTime.Size = New System.Drawing.Size(382, 20)
+        Me.dtpTime.TabIndex = 19
+        '
+        'dtpDay
+        '
+        Me.dtpDay.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dtpDay.CustomFormat = "dddd MMMM yyyy"
+        Me.dtpDay.Enabled = False
+        Me.dtpDay.Location = New System.Drawing.Point(139, 102)
+        Me.dtpDay.Name = "dtpDay"
+        Me.dtpDay.Size = New System.Drawing.Size(382, 20)
+        Me.dtpDay.TabIndex = 15
+        '
+        'btnBTL
+        '
+        Me.btnBTL.BackColor = System.Drawing.Color.FromArgb(CType(CType(182, Byte), Integer), CType(CType(171, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.btnBTL.FlatAppearance.BorderSize = 0
+        Me.btnBTL.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBTL.ForeColor = System.Drawing.SystemColors.Control
+        Me.btnBTL.Location = New System.Drawing.Point(56, 346)
+        Me.btnBTL.Name = "btnBTL"
+        Me.btnBTL.Size = New System.Drawing.Size(75, 23)
+        Me.btnBTL.TabIndex = 12
+        Me.btnBTL.Text = "Batal"
+        Me.btnBTL.UseVisualStyleBackColor = False
+        '
+        'btnSV
+        '
+        Me.btnSV.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSV.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnSV.FlatAppearance.BorderSize = 0
+        Me.btnSV.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSV.Location = New System.Drawing.Point(446, 346)
+        Me.btnSV.Name = "btnSV"
+        Me.btnSV.Size = New System.Drawing.Size(75, 23)
+        Me.btnSV.TabIndex = 11
+        Me.btnSV.Text = "Simpan"
+        Me.btnSV.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(26, 104)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Tanggal Masuk"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 31)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Jam Masuk"
+        '
+        'tbNO
+        '
+        Me.tbNO.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbNO.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.tbNO.Location = New System.Drawing.Point(139, 228)
+        Me.tbNO.MaxLength = 19
+        Me.tbNO.Name = "tbNO"
+        Me.tbNO.Size = New System.Drawing.Size(382, 20)
+        Me.tbNO.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NOPOL"
+        '
+        'tbHRG
+        '
+        Me.tbHRG.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbHRG.Location = New System.Drawing.Point(139, 168)
+        Me.tbHRG.MaxLength = 19
+        Me.tbHRG.Name = "tbHRG"
+        Me.tbHRG.ReadOnly = True
+        Me.tbHRG.Size = New System.Drawing.Size(382, 20)
+        Me.tbHRG.TabIndex = 23
+        Me.tbHRG.Text = "Rp. 3000"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Harga"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PictureBox4.Image = Global.AplikasiParkir.My.Resources.Resources.download_removebg_preview1
+        Me.PictureBox4.Location = New System.Drawing.Point(41, 328)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(133, 112)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 21
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(41, 142)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(133, 113)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 20
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(15, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(70, 71)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Motor
+        '
+        Me.AcceptButton = Me.btnSV
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 558)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.CRUD)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Motor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "user"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.CRUD.ResumeLayout(False)
+        Me.CRUD.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CRUD As GroupBox
+    Friend WithEvents dtpTime As DateTimePicker
+    Friend WithEvents dtpDay As DateTimePicker
+    Friend WithEvents btnBTL As Button
+    Friend WithEvents btnSV As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents tbNO As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents tbHRG As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ user.vb
@@ -0,0 +1,76 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Motor
+    Private Sub btnSV_Click(sender As Object, e As EventArgs) Handles btnSV.Click
+        Dim code As String = GenerateKode()
+        Dim tanggal As String = dtpDay.Value.ToString("yyyy:MM:dd")
+        Dim jam As String = dtpTime.Value.ToString("yyyy:MM:dd HH:mm:ss")
+        sql()
+        Dim cmd As New MySqlCommand("INSERT INTO parkings (transaction_id, plate_number, type_id, has_paid, time_in, date) " & "VALUES (@transaction_id, @plate, 1, 0, @time_in, @date_in)", conn)
+        cmd.Parameters.AddWithValue("@transaction_id", code)
+        cmd.Parameters.AddWithValue("@plate", tbNO.Text)
+        cmd.Parameters.AddWithValue("@time_in", jam)
+        cmd.Parameters.AddWithValue("@date_in", tanggal)
+        cmd.ExecuteNonQuery()
+
+        Dim cmdcd As New MySqlCommand("select counter from daily_counters where day = @day", conn)
+        cmdcd.Parameters.AddWithValue("@day", tanggal)
+        Dim cdr As Object = cmdcd.ExecuteScalar()
+
+        If cdr IsNot Nothing Then
+            ' Update jika sudah ada
+            Dim updateCounterQuery As String = "UPDATE daily_counters SET counter = counter + 1 WHERE day = @date"
+            Dim cmdUpdateCounter As New MySqlCommand(updateCounterQuery, conn)
+            cmdUpdateCounter.Parameters.AddWithValue("@date", tanggal)
+            cmdUpdateCounter.ExecuteNonQuery()
+        Else
+            ' Insert jika belum ada
+            Dim insertCounterQuery As String = "INSERT INTO daily_counters (day, counter) VALUES (@date, 1)"
+            Dim cmdInsertCounter As New MySqlCommand(insertCounterQuery, conn)
+            cmdInsertCounter.Parameters.AddWithValue("@date", tanggal)
+            cmdInsertCounter.ExecuteNonQuery()
+        End If
+
+        MessageBox.Show("Data Motor Tersimpan")
+        tbNO.Clear()
+        Me.ParentForm.Close()
+
+        conn.Close()
+    End Sub
+
+    Private Sub btnBTL_Click(sender As Object, e As EventArgs) Handles btnBTL.Click
+        tbNO.Clear()
+    End Sub
+
+    Private Function GenerateKode() As String
+        sql()
+        Dim existing_kodes As New List(Of Integer)
+        Dim query As String = "SELECT transaction_id FROM parkings WHERE transaction_id LIKE 'MTR____' ORDER BY transaction_id ASC"
+        Dim cmd As New MySqlCommand(query, conn)
+        Dim reader As MySqlDataReader = cmd.ExecuteReader()
+
+        While reader.Read()
+            Dim kode As String = reader("transaction_id").ToString()
+            Dim angka As Integer = 0
+            If Integer.TryParse(kode.Substring(kode.Length - 4), angka) Then
+                existing_kodes.Add(angka)
+            End If
+        End While
+        reader.Close()
+        conn.Close()
+
+        existing_kodes.Sort()
+
+        ' Cari angka terkecil yang belum dipakai
+        Dim new_kode As Integer = 1
+        For Each kode In existing_kodes
+            If kode <> new_kode Then
+                Exit For
+            End If
+            new_kode += 1
+        Next
+
+        Return "MTR" & new_kode.ToString("D4")
+    End Function
+
+End Class
