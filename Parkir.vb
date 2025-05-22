8013963eb1a512c362ea0b22360a45c2ad63132f
kapkaaa <kafkasanjaya123360@gmail.com>
2025-05-23 02:38:43 +0700

    Adding Parkir for panel motor and car

---
+++ Parkir.vb
@@ -0,0 +1,33 @@
+﻿Public Class Parkir
+
+    Private formtujuan As String
+
+    Public Sub New(tujuan As String)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        formtujuan = tujuan
+
+    End Sub
+    Private Sub Parkir_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If formtujuan = "motor" Then
+            LoadFormToPanel(New Motor())
+        ElseIf formtujuan = "mobil" Then
+            LoadFormToPanel(New Mobil())
+        End If
+
+    End Sub
+
+    Private Sub LoadFormToPanel(childForm As Form)
+        panelContainer.Controls.Clear()
+        With childForm
+            .TopLevel = False
+            .FormBorderStyle = FormBorderStyle.None
+            .Dock = DockStyle.Fill
+        End With
+        panelContainer.Controls.Add(childForm)
+        childForm.Show()
+    End Sub
+End Class
